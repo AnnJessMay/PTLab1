675fac608ebaced10374880e1fde196ce511f45e
AnnJessMay <yesjessmay@mail.ru>
2023-10-06 16:12:31 +0300

    create new DataReader

---
+++ src/DataReaderjson.py
@@ -0,0 +1,23 @@
+from Types import DataType
+from DataReader import DataReader
+import json
+
+class DataReaderJSON(DataReader):
+
+    def __init__(self) -> None:
+        self.students: DataType = {}        
+
+    def read(self, path: str) -> DataType:
+        with open(path, encoding='utf-8') as file:
+            data = json.load(file)
+            for fio in data:
+                temp_list = []
+                for subject in data[fio]:
+                    temp_list.append((subject, data[fio][subject]))
+                self.students[fio] = temp_list
+            return self.students
+
+
+if __name__ == '__main__':
+    data = DataReaderJSON().read('C:\\Users\\79616\\Desktop\\git\\PTLab1\\rating\\data\\data.json')
+    print(data)
