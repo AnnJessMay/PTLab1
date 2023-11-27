4f07ad52a759271057903bc08e68068671a78cee
AnnJessMay <yesjessmay@mail.ru>
2023-11-28 00:29:40 +0300

    make 2 variant

--- src/CalcQuartile.py
+++ src/CalcQuartile.py
@@ -14,7 +14,8 @@ class CalcQuartile:
         sorted_rating = sorted(rating.items(), key=lambda x: x[1],
                                reverse=True)
         quartile_rating = statistics.quantiles(sorted_rating.values())
-        quartile_rating2 = filter(lambda x: quartile_rating < x[1], sorted_rating)
+        quartile_rating2 = filter(lambda x: quartile_rating < x[1], 
+                                sorted_rating)
         return dict(quartile_rating2)
 
 
